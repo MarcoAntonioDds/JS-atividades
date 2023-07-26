829127202575df852f24e9f29cc829d0fab8524d
MarcoAntonioDds <127966364+MarcoAntonioDds@users.noreply.github.com>
2023-07-26 13:35:47 -0300

    Add files via upload

---
+++ nomequevcquiser.js
@@ -0,0 +1,36 @@
+//minha idade
+console.log(16)
+
+//Quantas semanas eu programei
+console.log("2 semanas")
+
+/*comentarios longos*/
+
+let foo = 24; 
+foo = "objeto" 
+foo = true; 
+foo = false;
+foo = 69
+foo = "objete"
+//minha idade mais 3.5
+console.log(16+3.5)
+
+//quantos anos se passaram desde 1969 até 2023
+console.log(2023-1969)
+
+//divisão de 65 por 240
+console.log(65/240)
+
+//multiplicação de 0.2708 por 100
+console.log(0.2708*100)
+
+console.log('hello'+'world')
+
+console.log('hello '+'world')
+
+console.log('Test'.length)
+console.log('sus'.toUpperCase());
+console.log(' nice game boy '.trim())
+console.log(100*Math.random())
+console.log(Math.ceil(43.8))
+console.log(Number.isInteger(2017))
